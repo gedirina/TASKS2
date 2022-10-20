9653153c7d88f60f26896324706de589d92dc088
Irina <gedirina25@gmail.com>
2022-10-20 14:42:18 +0300

    задача2

---
+++ задача2/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+if (n<100)  
+{ 
+    Console.WriteLine("Число двузначное");
+    }
+else 
+{
+    int coef=10;
+        while (true)
+{
+    int sep=n/coef;
+    if (sep>10 && sep<100)
+    {
+    break;}
+else
+{
+            coef *=10;
+}
+}
+Console.WriteLine($"Второе число - {(n/coef) %10}");
+}
+
