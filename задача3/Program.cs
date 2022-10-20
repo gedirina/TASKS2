7e973e983926261dd09717d0add8189ab2e0df08
Irina <gedirina25@gmail.com>
2022-10-20 15:11:35 +0300

    Задача3

---
+++ задача3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи номер дня недели: ");
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void DayOfTheWeek (int dayNumber)
+ {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("этот день выходной");
+  }
+ 
+  else Console.WriteLine("этот день не выходной");
+}
+
+DayOfTheWeek(dayNumber);
